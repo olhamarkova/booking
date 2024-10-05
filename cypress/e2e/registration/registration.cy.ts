fa432494713aec69ea3996fbbc3b0112d3725df2
olhamarkova <olga.mulchenko@gmail.com>
2024-10-05 18:53:50 +0300

    update beforeEach hook in the test

--- cypress/e2e/registration/registration.cy.ts
+++ cypress/e2e/registration/registration.cy.ts
@@ -22,53 +22,51 @@ describe("Registration with email", function () {
   beforeEach(function () {
     cy.goTo("");
     cy.setEngLanguage();
+
+    //Click on the "Register" button // this step repeats in every test, so I placed it here to follow the DRY principle
+    homePage.header.click(homePage.header.registerButton());
+    signIn.heading.hasText(signIn.title(), headings.signIn);
   });
 
   it("A user shall be able to register with valid email and password", function () {
-    //Click on the 'Register' button
-    homePage.header.click(homePage.header.registerButton());
-    signIn.heading.hasText(signIn.title(), headings.signIn);
-
     //Enter an email
     signIn.input.fill(signIn.emailInput(), email);
     signIn.input.hasValue(signIn.emailInput(), email);
 
-    //Click the 'Continue with email' button
+    //Click the "Continue with email" button
     signIn.button.click(signIn.submitButton());
     createPassword.heading.hasText(
       createPassword.title(),
       headings.createPassword
     );
 
-    //Enter the password in the 'Password' input field
+    //Enter the password in the "Password" input field
     createPassword.input.fill(createPassword.passwordInput(), password);
 
-    //Enter the same password in the 'Confirm password' input field
+    //Enter the same password in the "Confirm password" input field
     createPassword.input.fill(createPassword.confirmPasswordInput(), password);
 
-    //Click 'Create account' button
+    //Click "Create account" button
     createPassword.button.click(createPassword.submitButton());
+
+    //check if the captcha appears; if not, run test further, if yes - stop the test
     if (!createPassword.heading.isVisible(createPassword.captchaMessage())) {
       homePage.modal.isVisible(
         homePage.modal.getModalHeading(headings.registrationSuccessModal)
       );
 
-      //Click 'Got it!' button
+      //Click "Got it!" button
       homePage.modal.button.click(homePage.modal.getButton("Got it!"));
       homePage.header.isVisible(homePage.header.getProfile());
     } else return;
   });
 
   it("A user shall NOT be able to register with a compromised email", function () {
-    //Click on the 'Register' button
-    homePage.header.click(homePage.header.registerButton());
-    signIn.heading.hasText(signIn.title(), headings.signIn);
-
     //Enter an email
     signIn.input.fill(signIn.emailInput(), "test@gmail.com");
     signIn.input.hasValue(signIn.emailInput(), "test@gmail.com");
 
-    //Click the 'Continue with email' button
+    //Click the "Continue with email" button
     signIn.button.click(signIn.submitButton());
     disabledAccount.heading.hasText(
       disabledAccount.title(),
@@ -83,16 +81,12 @@ describe("Registration with email", function () {
   });
 
   it("A user shall see the error if uses invalid email for registration", function () {
-    //Click on the 'Register' button
-    homePage.header.click(homePage.header.registerButton());
-    signIn.heading.hasText(signIn.title(), headings.signIn);
-
     invalidEmails.forEach((invalidEmail) => {
       //Enter an email
       signIn.input.fill(signIn.emailInput(), invalidEmail);
       signIn.input.hasValue(signIn.emailInput(), invalidEmail);
 
-      //Click the 'Continue with email' button
+      //Click the "Continue with email" button
       signIn.button.click(signIn.submitButton());
       signIn.textBox.hasText(
         signIn.errorMessage(),
